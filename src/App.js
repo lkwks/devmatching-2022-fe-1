54a7d2c3c93e6821906e37c04fb7c9b3df8943c5
lkwks <lkwks@naver.com>
2022-09-06 15:38:13 +0700

    수정

--- src/App.js
+++ src/App.js
@@ -10,16 +10,24 @@ export default class App
         this.$target = $target;
         this.suggestion = new Suggestion($target)
         this.selectedlanguage = new SelectedLanguage($target)
-        this.searchinput = new SearchInput($target)
+        this.searchinput = new SearchInput($target.querySelector(".SearchInput__input"), this.suggestion, this.selectedlanguage)
     }
 }
 
 class SearchInput
 {
-    constructor($target)
+    constructor($target, suggestion, selectedlanguage)
     {
-        $target.querySelector(".SearchInput__input").addEventListener("input", function(){alert(1)})
-        //검색어 입력 시 suggestion을 렌더링하는 코드를 실행하는 이벤트 리스너
-        //검색어 다 지워지면 suggestion을 가리는 코드를 실행하는 이벤트 리스너
+        this.suggestion = suggestion
+        this.selectedlanguage = selectedlanguage
+        this.$target = $target
+        $target.addEventListener("input", this.keywordChange)
+        //검색어 입력 시 suggestion을 렌더링하는 코드를 실행하는 이벤트 리스너. 검색어 다 지워지면 저절로 suggestion이 가려지도록 코딩할 것.
+    }
+
+    keywordChange()
+    {
+        console.log(this.$target.value)
+        this.suggestion.keywordChange(this.$target.value)
     }
 }
